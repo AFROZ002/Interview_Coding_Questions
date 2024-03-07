ba5ed55aa167c699dff8a57e5620bad2a4772b6b
Afroz Alam <afrozcpr@gmail.com>
2024-03-08 02:42:20 +0530

    initial commit

--- First_Questions.java
+++ First_Questions.java
@@ -1,23 +1,23 @@
-package OOPS_Interview_Questions;
-
-class Base{
-    private void amethod(int iBase)
-{
-    System.out.println("Base.amethod");
-}
-}
-class Over extends Base{
-    public void amethod(int iOver){
-        System.out.println("Over.amethod");
-    }
-
-
-    public static void main(String[] args) {
-
-        Over o = new Over();
-        int iBase=0;
-        o.amethod(iBase);
-    }
-
-
-}
+//package OOPS_Interview_Questions;
+//
+//class Base{
+//    private void amethod(int iBase)
+//{
+//    System.out.println("Base.amethod");
+//}
+//}
+//class Over extends Base{
+//    public void amethod(int iOver){
+//        System.out.println("Over.amethod");
+//    }
+//
+//
+//    public static void main(String[] args) {
+//
+//        Over o = new Over();
+//        int iBase=0;
+//        o.amethod(iBase);
+//    }
+//
+//
+//}
--- Second_Questions.java
+++ Second_Questions.java
@@ -1,31 +1,31 @@
-package OOPS_Interview_Questions;
-
-class Base {
-    int i = 99;
-
-    public void amethod() {
-        System.out.println("Base.amethod()");
-    }
-
-    Base() {
-        amethod();
-    }
-}
- class Derived extends Base {
-    int i = -1;
-
-    public void amethod() {
-        System.out.println("Derived.amethod()");
-
-    }
-
-    public static void main(String[] args) {
-
-        {
-            Base b = new Derived();
-            System.out.println(b.i);
-            b.amethod();
-        }
-
-    }
-}
+//package OOPS_Interview_Questions;
+//
+//class Base {
+//    int i = 99;
+//
+//    public void amethod() {
+//        System.out.println("Base.amethod()");
+//    }
+//
+//    Base() {
+//        amethod();
+//    }
+//}
+// class Derived extends Base {
+//    int i = -1;
+//
+//    public void amethod() {
+//        System.out.println("Derived.amethod()");
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        {
+//            Base b = new Derived();
+//            System.out.println(b.i);
+//            b.amethod();
+//        }
+//
+//    }
+//}
